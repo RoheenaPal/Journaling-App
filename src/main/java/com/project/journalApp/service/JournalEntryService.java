6c3f867ad982ea383f2553accb6560d25e43b77b
Roheena Pal <palroheena@gmail.com>
2025-01-02 22:10:52 +0530

    connected mongoDB and modified the CRUD code

---
+++ src/main/java/com/project/journalApp/service/JournalEntryService.java
@@ -0,0 +1,35 @@
+package com.project.journalApp.service;
+
+import com.project.journalApp.entity.JournalEntry;
+import com.project.journalApp.repository.JournalEntryRepository;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Component
+public class JournalEntryService {
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry) {
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id) {
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id) {
+        journalEntryRepository.deleteById(id);
+    }
+}
